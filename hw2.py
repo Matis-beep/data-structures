141c8497d0380ef4c5ecf4837dda8376773dd61f
Matis-beep <mathis3441@gmail.com>
2026-03-03 19:08:46 +0000

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,41 @@
+def merge_sort_desc(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+
+        
+        left = arr[:mid]
+        right = arr[mid:]
+
+       
+        merge_sort_desc(left)
+        merge_sort_desc(right)
+
+        i = j = k = 0
+
+       
+        while i < len(left) and j < len(right):
+            if left[i] > right[j]:    
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+      
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+
+
+numbers = [38, 27, 43, 3, 9, 82, 10]
+merge_sort_desc(numbers)
+
+print(numbers)
