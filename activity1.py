387747895c5590a64f3c160965f5d589c53d93f6
Matis-beep <mathis3441@gmail.com>
2026-03-03 19:53:03 +0000

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self, n):
+        self.stack=[]
+        self.n=n
+
+    def push(self,k):
+        if len(self.stack)< self.n:
+            self.stack.append(k)
+        else:
+            print("the stack is full")
+    
+    def pop(self):
+        if len(self.stack) == 0:
+            print("the stack is empty")
+        else:
+            self.stack.pop(-1)
+
+    def top(self):
+        if len(self.stack):
+            print("the stack is empty")
+        else:
+            return self.stack[-1]
+    
+    def size(self):
+        return len(self.stack)
+    
+    def display(self):
+        print(self.stack)
+    
+s=Stack(3)
+s.display()
+print(s.size())
+s.push(5)
+s.display()
+print(s.size())
+s.push(10)
+s.display()
+print(s.size())
+s.push(15)
+s.display()
+print(s.size())
+s.push(25)
+s.display()
